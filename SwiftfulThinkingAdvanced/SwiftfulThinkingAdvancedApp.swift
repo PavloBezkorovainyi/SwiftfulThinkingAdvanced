ef2aa9acc3b270605df7f51c962aeed00d445472
Pavlo Bezkorovainyi <86742948+PavloBezkorovainyi@users.noreply.github.com>
2023-09-13 14:36:56 -0700

    Add files via upload

---
+++ SwiftfulThinkingAdvanced/SwiftfulThinkingAdvancedApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftfulThinkingAdvancedApp.swift
+//  SwiftfulThinkingAdvanced
+//
+//  Created by Павел Бескоровайный on 13.09.2023.
+//
+
+import SwiftUI
+
+@main
+struct SwiftfulThinkingAdvancedApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
